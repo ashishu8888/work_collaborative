21b7cc1fef81d15f048337fe2a06e2fd5154f4b7
Ashish <au2242524@gmail.com>
2022-10-13 19:09:00 +0530

    stored using local storage

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -5,11 +5,13 @@ import 'package:http/http.dart';
 import 'package:work_collaborative/constants/utils.dart';
 import 'package:work_collaborative/models/error_modal.dart';
 import 'package:work_collaborative/models/user_model.dart';
+import 'package:work_collaborative/repository/local_repository.dart';
 
 final AuthRepositoryProvider = Provider(
   (ref) => AuthRepository(
     googleSignIn: GoogleSignIn(),
     client: Client(),
+    localStorageRepository: LocalStorageRepository(),
   ),
 );
 
@@ -18,11 +20,14 @@ final userProvider = StateProvider<UserModel?>((ref) => null);
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   final Client _client;
+  final LocalStorageRepository _localStorageRepository;
   AuthRepository({
     required GoogleSignIn googleSignIn,
     required Client client,
+    required LocalStorageRepository localStorageRepository,
   })  : _googleSignIn = googleSignIn,
-        _client = client;
+        _client = client,
+        _localStorageRepository = localStorageRepository;
 
   Future<ErroModel> signinWithGoogle() async {
     ErroModel error = ErroModel(error: "some unexpected occured", data: null);
@@ -50,6 +55,7 @@ class AuthRepository {
               token: jsonDecode(res.body)['token'],
             );
             error = ErroModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
             break;
         }
         error;
@@ -60,36 +66,23 @@ class AuthRepository {
     return error;
   }
 
-  Future<ErroModel> signinWitxhGoogle() async {
+  Future<ErroModel> getUserData() async {
     ErroModel error = ErroModel(error: "some unexpected occured", data: null);
     try {
-      final user = await _googleSignIn.signIn();
-      if (user != null) {
-        final userAcc = UserModel(
-          email: user.email,
-          name: user.displayName!,
-          profilePic: user.photoUrl!,
-          uid: '',
-          token: '',
-        );
-        final res = await _client.post(
-          Uri.parse('$host/api/signup'),
-          body: userAcc.toJson(),
-          headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-        );
+      String? token = await _localStorageRepository.getToken();
+      if (token != null) {
+        var res = await _client.get(Uri.parse('$host/'), headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        });
         switch (res.statusCode) {
           case 200:
-            final newUser = userAcc.copyWith(
-              uid: jsonDecode(res.body)['user']['_id'],
-              token: jsonDecode(res.body)['token'],
-            );
-            error = ErroModel(error: null, data: newUser);
+            final newUser = UserModel.fromJson(res.body);
             break;
         }
-        error;
       }
+
+      error;
     } catch (e) {
       error = ErroModel(error: e.toString(), data: null);
     }
